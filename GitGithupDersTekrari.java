770c12e421bac2afbc9e11b8afda2fc537783353
Mihrican Pürbudak <mihricanpurbudak5@gmail.com>
2022-08-17 14:35:44 +0200

    V1

---
+++ GitGithupDersTekrari.java
@@ -0,0 +1,9 @@
+public class GitGithupDersTekrari {
+    /*
+    git init
+    git add .
+    git push
+    ilk sefer icin git remote add origin adres
+                   git push -u origin master
+     */
+}
